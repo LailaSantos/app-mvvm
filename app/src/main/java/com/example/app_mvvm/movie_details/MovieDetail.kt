5ad6769a8d7548785bb0a7323f2cac816f26cade
Laila Santos <lailaroberta100@gmail.com>
2022-08-25 23:14:18 -0300

    :package: adding folders from mvvm architecture

--- app/src/main/java/com/example/app_mvvm/movie_details/MovieDetail.kt
+++ app/src/main/java/com/example/app_mvvm/movie_details/MovieDetail.kt
@@ -1,12 +1,12 @@
-package com.example.app_mvvm
+package com.example.app_mvvm.movie_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.app_mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_detail)
     }
 }
